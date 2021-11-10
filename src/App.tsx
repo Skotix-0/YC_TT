d45c22ddeb4a7f80c454763e965ff7974d88121a
Skotix <dremal527@gmail.com>
2021-11-11 01:53:23 +0500

    change save funtional, and it will be grate, but functional delete is not a rerender component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.scss';
-import Render_table from './components/Render_table';
-import AdRender_table from './components/AdRender_table';
+import RENDER_TABLE from './components/Render_table';
+import ADRENDER_TABLE from './components/AdRender_table';
 
 function App() {
 
@@ -15,16 +15,16 @@ function App() {
     ati_code: string
   }
 
-  const [table_data, setTableData] = useState([
-    {
-      number_statement: 1,
-      date_statement: "Tue Nov 09 2021 16:15:34",
-      name_company: "Test",
-      fio_transporter: "VV PUTIN",
-      phone_number_transporter: "880005553535",
-      commit: "фыв",
-      ati_code: "12345"
-    },
+  const [table_data, setTableData]: any[] = useState([
+    // {
+    //   number_statement: 1,
+    //   date_statement: "Tue Nov 09 2021 16:15:34",
+    //   name_company: "Test",
+    //   fio_transporter: "VV PUTIN",
+    //   phone_number_transporter: "880005553535",
+    //   commit: "фыв",
+    //   ati_code: "12345"
+    // }
   ]);
 
   const [ADM_value, setADM_value] = useState(false);
@@ -32,14 +32,28 @@ function App() {
   const add_new_statement = (props: any):void =>{
     props.preventDefault();
 
-    let newObj:ITableData = {
-      number_statement: table_data.length + 1,
-      date_statement: `${props.target.elements.date_statement.value}`,
-      name_company: `${props.target.elements.name_company.value}`,
-      fio_transporter: `${props.target.elements.fio_transporter.value}`,
-      phone_number_transporter: `${props.target.elements.phone_number_transporter.value}`,
-      commit: `${props.target.elements.commit.value}`,
-      ati_code: `${props.target.elements.ati_code.value}`
+    let newObj:ITableData;
+
+    if(table_data.length <= 0){
+      newObj= {
+        number_statement: 1,
+        date_statement: `${props.target.elements.date_statement.value}`,
+        name_company: `${props.target.elements.name_company.value}`,
+        fio_transporter: `${props.target.elements.fio_transporter.value}`,
+        phone_number_transporter: `${props.target.elements.phone_number_transporter.value}`,
+        commit: `${props.target.elements.commit.value}`,
+        ati_code: `${props.target.elements.ati_code.value}`
+      }
+    }else{
+      newObj = {
+        number_statement: table_data[table_data.length - 1].number_statement + 1,
+        date_statement: `${props.target.elements.date_statement.value}`,
+        name_company: `${props.target.elements.name_company.value}`,
+        fio_transporter: `${props.target.elements.fio_transporter.value}`,
+        phone_number_transporter: `${props.target.elements.phone_number_transporter.value}`,
+        commit: `${props.target.elements.commit.value}`,
+        ati_code: `${props.target.elements.ati_code.value}`
+      }
     }
 
     setTableData([...table_data, newObj]);
@@ -50,34 +64,29 @@ function App() {
   }
 
   const DeleteStatement = (props:any):void =>{
-    table_data.splice(props-1, 1);
+    table_data.splice(props, 1)
     setTableData([...table_data]);
   }
 
   const SaveEditingStatement = (props: any):void =>{
-    let arr_elem = table_data[props[0]-1];
-    console.log(arr_elem);
-    console.log(props);
-    arr_elem.date_statement = props[1];
-    arr_elem.name_company = props[2];
-    arr_elem.fio_transporter = props[3];
-    arr_elem.phone_number_transporter = props[4];
-    arr_elem.commit = props[5];
-    arr_elem.ati_code = props[6];
-
+    table_data[props.id].date_statement = props.Obj.date_statement;
+    table_data[props.id].name_company = props.Obj.name_company;
+    table_data[props.id].fio_transporter = props.Obj.fio_transporter;
+    table_data[props.id].phone_number_transporter = props.Obj.phone_number_transporter;
+    table_data[props.id].commit = props.Obj.commit;
+    table_data[props.id].ati_code = props.Obj.ati_code;
     setTableData([...table_data]);
   } 
 
   return (
     <div className="App">
       { ADM_value === false &&
-        <Render_table table_arr = {table_data} ANS = {add_new_statement} ADM = {EnableAdminMode} />
+        <RENDER_TABLE table_arr = {table_data} ANS = {add_new_statement} ADM = {EnableAdminMode} />
       }
 
-      { ADM_value !== false &&
-        <AdRender_table table_arr = {table_data} SES = {SaveEditingStatement} ANS = {add_new_statement} ADM = {EnableAdminMode} Del = {DeleteStatement} />
+      { ADM_value !== false && 
+        <ADRENDER_TABLE table_arr = {table_data} SES = {SaveEditingStatement} ANS = {add_new_statement} ADM = {EnableAdminMode} Del = {DeleteStatement} />
       }
-      
     </div>
   );
 }
--- src/components/AdRender_table.tsx
+++ src/components/AdRender_table.tsx
@@ -18,18 +18,25 @@ export default function AdRender_table(props: any) {
         {title: "ФИО перевозчика", type: "text", name: "fio_transporter"},
         {title: "Телефон перевозчика", type: "text", name: "phone_number_transporter"},
         {title: "Комментарии", type: "text", name: "commit"},
-        {title: "Код сети перевозчика", type: "text", name: "ati_code"}
+        {title: "Код сети перевозчика", type: "text", name: "ati_code"},
+        {title: "", type: "", name: ""},
     ];
 
     let Progress: any[] = [];
 
     const EditRow = (props: any): void =>{
-        let child = Progress[props].children;
-        for(let i = 0; i < child.length-1; i++){
-            child[i].children[0].style.border = "1px solid black";
-            child[i].children[0].style.background = 'white';
-            child[i].children[0].removeAttribute('readOnly');
-        }
+        let child_array = [...Progress[props].children].map((elem: any) =>{
+            return [...elem.children];
+        });
+
+        child_array.map((elem: any) =>{
+            if(elem[0].classList.contains("ADM_table")){
+                elem[0].style.border = "1px solid black";
+                elem[0].style.background = 'white';
+                elem[0].removeAttribute('readOnly');
+            }
+            return true;
+        });
     }
     
     return (
@@ -41,7 +48,7 @@ export default function AdRender_table(props: any) {
                     statement_info_form.map((elem: any, key: number)=>{
                         return <div key={elem.title} className="form_input_div">
                                 <span>{elem.title}</span>
-                                <input type={elem.type} name={elem.name} id={"form_add_"+elem.name} />
+                                <input type={elem.type} name={elem.name} id={"form_add_"+elem.name} required/>
                             </div>;
                     })
                 }
@@ -60,7 +67,7 @@ export default function AdRender_table(props: any) {
                     {
                         props.table_arr.map((elem:any, key: number)=>{
                         return <tr key={key} id={"ADM_table"+elem.number_statement} ref={(input) => {Progress[elem.number_statement] = input }}>
-                                <td><input className="ADM_table" defaultValue={elem.number_statement} readOnly /></td>
+                                <td><input className="ADM_tableId" defaultValue={elem.number_statement} readOnly /></td>
                                 <td><input className="ADM_table" defaultValue={elem.date_statement} readOnly /></td>
                                 <td><input className="ADM_table" defaultValue={elem.name_company} readOnly /></td>
                                 <td><input className="ADM_table" defaultValue={elem.fio_transporter} readOnly /></td>
@@ -70,7 +77,7 @@ export default function AdRender_table(props: any) {
                                 <td>
                                     <div id={'Tool_'+elem.number_statement}>
                                         <button onClick={()=>{
-                                            props.Del(elem.number_statement);
+                                            props.Del(key);
                                         }}>Del</button>
                                         <button onClick={()=>{
                                             EditRow(elem.number_statement);
@@ -78,12 +85,36 @@ export default function AdRender_table(props: any) {
                                     </div>
                                     <div id={"Save_"+elem.number_statement}>
                                         <button onClick={()=>{
-                                            let child = Progress[elem.number_statement].children;
-                                            let arr_data = [];
-                                            for(let i = 0; i < child.length-1; i++){
-                                                arr_data.push(child[i].children[0].value);
+                                            let child_array = [...Progress[elem.number_statement].children].map((elem: any) =>{
+                                                return [...elem.children];
+                                            });
+                                    
+                                            let arr_value = child_array.map((elem: any) =>{
+                                                if(elem[0].classList.contains("ADM_table")){
+                                                    return elem[0].value;
+                                                }
+                                                return true;
+                                            });
+
+                                            let newObj = {
+                                                date_statement: arr_value[1],
+                                                name_company: arr_value[2],
+                                                fio_transporter: arr_value[3],
+                                                phone_number_transporter: arr_value[4],
+                                                commit: arr_value[5],
+                                                ati_code: arr_value[6],
                                             }
-                                            props.SES(arr_data);
+
+                                            child_array.map((elem: any) =>{
+                                                if(elem[0].classList.contains("ADM_table")){
+                                                    elem[0].style.border = "none";
+                                                    elem[0].style.background = 'none';
+                                                    elem[0].setAttribute('readOnly','readOnly');
+                                                }
+                                                return true;
+                                            });
+                                            
+                                            props.SES({Obj:newObj, id: elem.number_statement-1});
                                         }}>Save</button>
                                     </div>
                                 </td>
